4a5c813a9a22fec526a23c739cdb0731c9277c32
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2022-01-30 21:07:22 -0500

    update

--- Assets/Scripts/Neuro Apps/NeuroComputerGUI.cs
+++ Assets/Scripts/Neuro Apps/NeuroComputerGUI.cs
@@ -39,8 +39,8 @@ public class NeuroComputerGUI : MonoBehaviour, IPointerEnterHandler
                 var appType = appComp.GetType();
                 newApp.AddComponent(appType);
 
-                if (newApp.GetComponent<NeuroImplantApp>() != null)
-                    cloudAppButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = newApp.GetComponent<NeuroImplantApp>().appName;
+                //if (newApp.GetComponent<NeuroImplantApp>() != null)
+                    //cloudAppButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = newApp.GetComponent<NeuroImplantApp>().appName;
             }
             currentSelectedApp = computerDevice.cloudApps[0];
         }
