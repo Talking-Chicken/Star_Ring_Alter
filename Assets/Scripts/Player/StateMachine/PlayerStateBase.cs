9fadfdd7b78471eb5243f1c8afa57418b743b9f4
Leo Ji <sj3026@nyu.edu>
2022-02-14 17:01:29 -0500

    player state machine initiate

---
+++ Assets/Scripts/Player/StateMachine/PlayerStateBase.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerStateBase
+{
+    public abstract void EnterState(PlayerControl player);
+    public abstract void UpdateState(PlayerControl player);
+    public abstract void FixedupdateState(PlayerControl player);
+    public abstract void LeaveState(PlayerControl player);
+}
---
+++ Assets/Scripts/Player/StateMachine/PlayerStateExplore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateExplore : PlayerStateBase
+{
+    public override void EnterState(PlayerControl player) {
+
+    }
+    public override void UpdateState(PlayerControl player) {
+
+    }
+    public override void FixedupdateState(PlayerControl player) {
+
+    }
+    public override void LeaveState(PlayerControl player) {
+
+    }
+}
