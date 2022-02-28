eb7c4d585a6a3f3a87529f0ca37fda1fd2663339
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2022-02-28 15:05:45 -0500

    push

---
+++ Assets/dynamic_camera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dynamic_camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera main_camera;
+    public float static_fov;
+    public float moving_fov;
+    private float t;
+    private float i;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (IsometricPlayerMovementController.movement==new Vector2(0,0)) {
+
+            i = 0;
+            main_camera.orthographicSize = Mathf.Lerp(moving_fov, static_fov, t);
+            t += 2f * Time.deltaTime;
+
+        }
+        else
+        {
+            t = 0;
+            main_camera.orthographicSize = Mathf.Lerp(static_fov, moving_fov, i);
+           i += 2f * Time.deltaTime;
+        }
+     
+
+
+
+    }
+}
