93b28f5369ec93933dd28614bf13c7b3856ea45d
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2022-03-06 18:23:25 -0500

    push

---
+++ Assets/field_comm_control.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class field_comm_control : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject player;
+    Animator anim;
+    bool playonce = true;
+    bool playonce1 = true;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector2.Distance(player.transform.position, transform.position);
+        if (playonce&&dist <= 10 && !anim.GetCurrentAnimatorStateInfo(0).IsName("field_comm_opened")) 
+        { anim.Play("field_comm_opening"); playonce = false; }
+
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("field_comm_opened")) {
+            playonce1 = true;
+            playonce = true;
+        }
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("field_comm_closed"))
+        {
+           
+            playonce = true;
+            playonce1 = true;
+
+        }
+        else if (playonce1&&dist > 10 && !anim.GetCurrentAnimatorStateInfo(0).IsName("field_comm_closed"))
+        {
+            anim.Play("field_comm_closing");
+            playonce1 = false;
+        }
+
+    }
+}
---
+++ Assets/pipeline_changing.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class pipeline_changing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject moon_lander;
+    public RenderPipelineAsset standard;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moon_lander.activeSelf)
+        {
+            UnityEngine.Rendering.GraphicsSettings.renderPipelineAsset = null;
+        }
+        else { UnityEngine.Rendering.GraphicsSettings.renderPipelineAsset = standard; }
+    }
+}
