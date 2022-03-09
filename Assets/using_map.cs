d094706ab52400cce769a57a6344b805b0d2ef65
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2022-03-08 22:57:30 -0500

    push

---
+++ Assets/using_map.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class using_map : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool map_on;
+    public GameObject map;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerControl.CanMove && Input.GetKey(KeyCode.M)) {
+            map.SetActive(true);
+            PlayerControl.CanMove = false;
+        }
+
+        if (map.activeSelf && Input.GetKey(KeyCode.M)) {
+            map.SetActive(false);
+            PlayerControl.CanMove = true;
+        }
+    }
+}
