a97b37f2626d3385f4d3e4ab252e53fa690671cb
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2022-04-12 22:48:15 -0400

    push

--- Assets/random_conversation.cs
+++ Assets/random_conversation.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class random_conversation : MonoBehaviour
 {
     // Start is called before the first frame update
+    public TextAsset file;
+    public string[] lines;
     void Start()
     {
-        
+        readCSV();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void readCSV()
+    {
+       lines = file.text.Split("\n"[0]);
+        for (var i = 0; i < lines.Length; i++)
+        {
+            string[] parts = lines[i].Split(","[0]);
+        }
     }
 }
