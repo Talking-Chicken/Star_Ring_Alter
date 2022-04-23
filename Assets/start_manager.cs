f8a44c4acab9a032a4dcfad9533ce1c4e18c6a90
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2022-04-22 22:18:14 -0400

    start scene

--- Assets/start_manager.cs
+++ Assets/start_manager.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class start_manager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject ControlPanel;
+    public GameObject CreditPanel;
+    public AudioSource UI_SE;
+    public AudioClip clicked;
     void Start()
     {
         
@@ -14,5 +19,30 @@ public class start_manager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void NewGame() 
+    {
+        SceneManager.LoadScene("Main");
+        UI_SE.PlayOneShot(clicked);
+    }
+    public void ControlButton()
+    {
+        ControlPanel.SetActive(true);
+        UI_SE.PlayOneShot(clicked);
+    }
+    public void ControlEXIT()
+    {
+        ControlPanel.SetActive(false);
+        UI_SE.PlayOneShot(clicked);
+    }
+    public void CreditButton()
+    {
+        CreditPanel.SetActive(true);
+        UI_SE.PlayOneShot(clicked);
+    }
+    public void CreditEXIT()
+    {
+        CreditPanel.SetActive(false);
+        UI_SE.PlayOneShot(clicked);
     }
 }
