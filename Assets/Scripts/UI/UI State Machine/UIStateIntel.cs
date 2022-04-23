16962300567fe66da40ed674b07ca8ee9c5c0d51
Leo Ji <sj3026@nyu.edu>
2022-04-23 00:32:23 -0400

    map tab now is in working

---
+++ Assets/Scripts/UI/UI State Machine/UIStateIntel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIStateIntel : UIStateBase
+{
+    public override void EnterState(UIControl UI)
+    {
+        
+    }
+
+    public override void UpdateState(UIControl UI)
+    {
+        
+    }
+
+    public override void LeaveState(UIControl UI)
+    {
+        
+    }
+}
