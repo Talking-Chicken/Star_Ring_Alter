d74b71cca6f0fac98ea393981b09578a097c015d
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2022-04-23 14:12:07 -0400

    save  system

--- Assets/Scripts/GameManager/TimeLoopSystem.cs
+++ Assets/Scripts/GameManager/TimeLoopSystem.cs
@@ -45,6 +45,7 @@ public class TimeLoopSystem : MonoBehaviour
 
         DateTime today = DateTime.Today;
         Time_text.time_2 = new DateTime(today.Year, today.Month, today.Day, 0, 19, 0);
+        ES3.Save("Condition",random_conversation.lines);
         SceneManager.LoadScene("cut_scene_2");
     }
 }
