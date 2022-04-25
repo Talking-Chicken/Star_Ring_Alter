fe1a797b4f3db9e304c6a36069fc0c09d73a2d15
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2022-04-25 17:10:16 -0400

    more_interaction_details

--- Assets/Scripts/robot_console.cs
+++ Assets/Scripts/robot_console.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class robot_console : InteractiveObj
 {
     public GameObject robot;
+    [SerializeField] AudioClip clip;
+    [SerializeField] AudioSource audio;
     public override void interact()
     {
-        if (FindObjectOfType<DeliveryTunnel>().sentRobot)
-            robot.SetActive(true);
+        audio.PlayOneShot(clip);
+        if (FindObjectOfType<DeliveryTunnel>().sentRobot) { robot.SetActive(true); StartCoroutine(waitToChangeToExploreState()); } else
+        {
+            PlayerControl player = FindObjectOfType<PlayerControl>();
+            player.ChangeState(player.stateExplore);
+            player.talkToSelf("Response.robot_control");
+        }
+          
         
-        StartCoroutine(waitToChangeToExploreState());
+        
     }
 }
