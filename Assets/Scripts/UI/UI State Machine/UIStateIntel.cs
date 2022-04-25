8b3a3d7bbb6852af6f68aa8c6fd724b939ccf8d9
Leo Ji <sj3026@nyu.edu>
2022-04-24 23:04:42 -0400

    linked intel to UI control

--- Assets/Scripts/UI/UI State Machine/UIStateIntel.cs
+++ Assets/Scripts/UI/UI State Machine/UIStateIntel.cs
@@ -6,16 +6,22 @@ public class UIStateIntel : UIStateBase
 {
     public override void EnterState(UIControl UI)
     {
-        
+        UI.openGUI += UI.openIntel;
+        UI.closeGUI += UI.closeIntel;
+        UI.openGUI();
     }
 
     public override void UpdateState(UIControl UI)
     {
-        
+        if (Input.GetKeyDown(UI.Key.previous)) {
+            UI.ChangeState(UI.stateMap);
+        }
     }
 
     public override void LeaveState(UIControl UI)
     {
-        
+        UI.closeGUI();
+        UI.openGUI -= UI.openIntel;
+        UI.closeGUI -= UI.closeIntel;
     }
 }
