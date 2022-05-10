f78298d373c6fe79700c49358eb3235077ce5b6a
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2022-05-10 02:54:04 -0400

    memory_1

---
+++ Assets/Scripts/Yarn_cutscene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class Yarn_cutscene : MonoBehaviour
+{
+    [YarnCommand("Cutscene")]
+    public void Cutscene()
+    {
+        
+        cut_scene_dialogue_manager.cutscene++;
+
+    }
+   
+}
--- Assets/cut_scene_dialogue_manager.cs
+++ Assets/cut_scene_dialogue_manager.cs
@@ -8,6 +8,12 @@ public class cut_scene_dialogue_manager : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] string[] line;
    [SerializeField] DialogueRunner runner;
+   public static int cutscene = 0;
+    string looptime;
+    [SerializeField] GameObject video;
+    [SerializeField] GameObject memory_1;
+    [SerializeField] AudioSource audio;
+    [SerializeField] Camera camera;
     void Start()
     {
         line = ES3.Load<string[]>("Condition1", "Star_Ring_Save/myFile.es3");
@@ -18,22 +24,68 @@ public class cut_scene_dialogue_manager : MonoBehaviour
 
             if (parts[0].Equals("loop_time"))
             {
-                if (parts[2].Equals("1")) { runner.startNode = "Random_dialogue.End_of_loop"; }
-                else { runner.startNode = "Random_dialogue.End_of_loop_1"; }
+                looptime = parts[2];
+              
             }
-
-            
-
-
-
-
-
             // Update is called once per frame
-           
+
         }
     }
-    void Update()
+    private void Update()
     {
+        Cutscene();
+    }
+    void Cutscene()
+    {
+        switch (cutscene)
+        {
+            case (0):  
+                {
+                    if (looptime.Equals("1")) { runner.startNode = "Random_dialogue.End_of_loop"; }
+                    else { runner.startNode = "Random_dialogue.End_of_loop_1"; }
+                   // cutscene = 1;
+                    break;//Intro
+                }
 
+            case (1):
+                {
+                  
+                    video.SetActive(false);memory_1.SetActive(true);
+                    cutscene = 2;
+                    break;//Intro
+                }
+            case (2):
+                {
+
+                    camera.GetComponent<FadeCamera>().FadeOut(2f);
+                    runner.startNode = "Random_dialogue.Memory";
+                    runner.StartDialogue();
+                    cutscene = 3;
+                    break;//Intro
+                }
+            case (3):
+                {
+
+                    if (runner.IsDialogueRunning==false) { 
+                        //press space
+                        cutscene = 4; }
+                    
+                    break;//Intro
+                }
+            case (4):
+                {
+
+                    if (Input.GetKey(KeyCode.Space))
+                    {
+                       memory_1.GetComponent<Animator>().Play("memory_1");
+                        runner.startNode = "Random_dialogue.Memory_1";
+                        runner.StartDialogue();
+                        cutscene = 5;
+                    }
+
+                    break;//Intro
+                }
+            case (5): { break; }
+        }
     }
 }
