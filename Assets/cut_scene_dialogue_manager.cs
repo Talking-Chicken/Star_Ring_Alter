114bd1eed1f6961988809fe80daeb70f95bad9b0
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2022-05-10 19:46:37 -0400

    new_memory

--- Assets/cut_scene_dialogue_manager.cs
+++ Assets/cut_scene_dialogue_manager.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
+using UnityEngine.Rendering;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class cut_scene_dialogue_manager : MonoBehaviour
 {
@@ -12,10 +15,17 @@ public class cut_scene_dialogue_manager : MonoBehaviour
     string looptime;
     [SerializeField] GameObject video;
     [SerializeField] GameObject memory_1;
+    [SerializeField] GameObject memory_2;
     [SerializeField] AudioSource audio;
     [SerializeField] Camera camera;
+    [SerializeField] TextMeshProUGUI interaction_text;
+    [SerializeField] AudioClip surgical;
+    [SerializeField] AudioClip light_off;
+    [SerializeField] AudioClip robot;
+    [SerializeField] AudioClip door;
     void Start()
     {
+        interaction_text.text = null;
         line = ES3.Load<string[]>("Condition1", "Star_Ring_Save/myFile.es3");
         for (var i = 0; i < line.Length; i++)
         {
@@ -30,6 +40,7 @@ public class cut_scene_dialogue_manager : MonoBehaviour
             // Update is called once per frame
 
         }
+        GraphicsSettings.defaultRenderPipeline = null;
     }
     private void Update()
     {
@@ -49,43 +60,125 @@ public class cut_scene_dialogue_manager : MonoBehaviour
 
             case (1):
                 {
-                  
-                    video.SetActive(false);memory_1.SetActive(true);
+                    camera.GetComponent<FadeCamera>().FadeOut(2f);
+                    audio.Stop();
+                    StartCoroutine(ExampleCoroutine());
                     cutscene = 2;
+
                     break;//Intro
                 }
             case (2):
                 {
 
-                    camera.GetComponent<FadeCamera>().FadeOut(2f);
-                    runner.startNode = "Random_dialogue.Memory";
-                    runner.StartDialogue();
-                    cutscene = 3;
+                    
                     break;//Intro
                 }
             case (3):
                 {
+                    if (looptime.Equals("2"))
+                    {
+                        runner.startNode = "Random_dialogue.Memory_2";
+                        runner.StartDialogue();
+                        StartCoroutine(ExampleCoroutine2());
+                        audio.PlayOneShot(door);
+                        cutscene = 4;
+                        StartCoroutine(ExampleCoroutine1());
+                    }
+                    else if (looptime.Equals("1"))
+                    {
+                        runner.startNode = "Random_dialogue.Memory";
+                        //   audio.PlayOneShot(surgical);
+                        audio.clip = surgical;
+                        audio.Play();
+                        runner.StartDialogue();
+                        cutscene = 4;
+                        StartCoroutine(ExampleCoroutine1());
+                    }
+                    else { SceneManager.LoadScene("Main"); }
+                        
 
-                    if (runner.IsDialogueRunning==false) { 
-                        //press space
-                        cutscene = 4; }
-                    
                     break;//Intro
                 }
             case (4):
                 {
+                   
+                    if (runner.IsDialogueRunning==false) {
+                        //press space
+                        camera.GetComponent<FadeCamera>().FadeIn(2f);
+
+                        
+                    }
+                    
+                    break;//Intro
+                }
+            case (5):
+                {
+
+
+
+                    if (looptime.Equals("2"))
+                    { interaction_text.text = "Press Space to turn on Desk Lamp"; }
+                    else if (looptime.Equals("1"))
+                    {
+                        interaction_text.text = "Press Space to turn off Surgical Light";
+                    }
+                    cutscene = 6;
+                    break;//Intro
+                }
+            case (6):
+                {
 
                     if (Input.GetKey(KeyCode.Space))
                     {
-                       memory_1.GetComponent<Animator>().Play("memory_1");
+                        interaction_text.text = "";
+                        audio.PlayOneShot(light_off);
+                        audio.PlayOneShot(robot);
+                        memory_1.GetComponent<Animator>().Play("memory_1");
                         runner.startNode = "Random_dialogue.Memory_1";
                         runner.StartDialogue();
-                        cutscene = 5;
+                        cutscene = 7;
                     }
 
                     break;//Intro
                 }
-            case (5): { break; }
+            case (7): {
+                    //camera.GetComponent<FadeCamera>().FadeOut(2f);
+                    break; }
+            case (8):
+                {
+                   // camera.GetComponent<FadeCamera>().FadeOut(2f);
+                    break;
+                }
         }
+    }
+    IEnumerator ExampleCoroutine()
+    {
+        
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(6);
+        video.SetActive(false); memory_1.SetActive(true);
+
+        cutscene = cutscene + 1;
+    }
+    IEnumerator ExampleCoroutine1()
+    {
+
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(6);
+        
+
+        cutscene = cutscene + 1;
+    }
+    IEnumerator ExampleCoroutine2()
+    {
+
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(6);
+        video.SetActive(false); memory_2.SetActive(true);
+
+        cutscene = cutscene + 1;
     }
 }
