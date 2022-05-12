412bc258af5c413cceedbf16ca3b4db91f06de4f
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2022-05-12 17:07:32 -0400

    minor_change

--- Assets/access_manager.cs
+++ Assets/access_manager.cs
@@ -7,11 +7,29 @@ public class access_manager : MonoBehaviour
     // Start is called before the first frame update
     PlayerBackpack playerBackpack;
     public int access_level;
+    string[] parts;
     void Start()
     {
         playerBackpack = FindObjectOfType<PlayerBackpack>();
-    }
+        for (var i = 0; i < random_conversation.lines.Length; i++)
+        {
+            parts = random_conversation.lines[i].Split(',');
+            parts[0] = parts[0].Replace("\r", "");
 
+            if (parts[0].Equals("sec_station_checked"))
+            {
+
+                if (parts[2].Equals("TRUE"))
+                {
+
+
+                    access_level = 2;
+                    break;
+                }
+
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
     {
