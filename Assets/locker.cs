4feb8943d357bd117614b682c9d6e1fef0381e8e
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2022-05-12 20:08:09 -0400

    hint for spy terminal

--- Assets/locker.cs
+++ Assets/locker.cs
@@ -2,17 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class locker : MonoBehaviour
+public class locker : InteractiveObj
 {
-    // Start is called before the first frame update
-    void Start()
+    string[] parts;
+    PlayerBackpack playerBackpack;
+    [SerializeField]GameObject hint;
+    [SerializeField] AudioClip pickup;
+    [SerializeField] AudioSource audio;
+    private void Start()
     {
-        
+        playerBackpack = FindObjectOfType<PlayerBackpack>();
     }
-
-    // Update is called once per frame
-    void Update()
+    public override void interact()
     {
-        
+        PlayerControl player = FindObjectOfType<PlayerControl>();
+        player.ChangeState(player.stateExplore);
+        if (playerBackpack.contains("Access Card".ToLower().Trim()))
+        { player.talkToSelf("Investigate.12"); }
+        else { player.talkToSelf("Investigate.6"); }
+
+    }
+    public override void useItem()
+    {
+        Item currentItem = InventoryGUIControl.currentUnit.items.Peek();
+        PlayerControl player = FindObjectOfType<PlayerControl>();
+
+
+        if (currentItem.ItemName.ToLower().Trim().Contains("Access Card".ToLower().Trim()))
+        {
+            player.ChangeState(player.stateExplore);
+            player.talkToSelf("Investigate.14");
+            playerBackpack.add(hint);
+            audio.PlayOneShot(pickup);
+            hint.SetActive(false);
+           
+        }
+        else
+        {
+            player.ChangeState(player.stateExplore);
+            player.talkToSelf("Investigate.13");
+        }
     }
 }
---
+++ Assets/strange_message.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class strange_message : Item
+{
+    public override void useItem()
+    {
+        PlayerControl player = FindObjectOfType<PlayerControl>();
+        player.ChangeState(player.stateExplore);
+        player.talkToSelf("Investigate.15");
+    }
+}
