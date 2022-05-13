f3b31c9cbccb814b52a4cf9a567f0733d2a67574
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2022-05-13 05:45:58 -0400

    minor hot fix

--- Assets/Scripts/Iteractables/NeuroImplantAdjuster.cs
+++ Assets/Scripts/Iteractables/NeuroImplantAdjuster.cs
@@ -8,6 +8,8 @@ public class NeuroImplantAdjuster : InteractiveObj
     private UIControl uiControl;
     [SerializeField] GameObject adjusterCamera;
     [SerializeField] GameObject adjusterUIContainer;
+    string[] parts;
+    bool first;
     private void Start()
     {
         UI = GetComponentInParent<NeuroComputerGUI>();
@@ -26,13 +28,31 @@ public class NeuroImplantAdjuster : InteractiveObj
         //     UI.showUI();
         // }
         PlayerControl player = FindObjectOfType<PlayerControl>();
+        for (var i = 0; i < random_conversation.lines.Length; i++)
+        {
+            parts = random_conversation.lines[i].Split(',');
+            parts[0] = parts[0].Replace("\r", "");
 
-        player.ChangeState(player.stateUI);
-        player.talkToSelf("Response.neural_implant_adjuster");
-      
+            if (parts[0].Equals("hacking_module") && parts[2].Equals("FALSE"))
+            {
+                first = true;
+                random_conversation.lines[i] = parts[0] + "," + parts[1] + "," + "TRUE" + "," + parts[3];
+                break;
+            }
+            else if (parts[0].Equals("hacking_module") && parts[2].Equals("TRUE")) { first = false; break; }
+        }
+        if (first) {
+            player.ChangeState(player.stateExplore);
+            player.talkToSelf("adjuster_first");
+        } else {
+            player.ChangeState(player.stateUI);
+            player.talkToSelf("Response.neural_implant_adjuster");
 
-        Camera.main.gameObject.SetActive(false);
-        adjusterCamera.SetActive(true);
-        adjusterUIContainer.SetActive(true);
+
+            Camera.main.gameObject.SetActive(false);
+            adjusterCamera.SetActive(true);
+            adjusterUIContainer.SetActive(true);
+        }
+        
     }
 }
