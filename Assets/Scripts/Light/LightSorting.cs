14c3627db71a7abd04864fde06510a8b8b21e61c
WenxuanLiao <wl1884@nyu.edu>
2022-09-11 17:21:24 -0400

    refrigerator light

---
+++ Assets/Scripts/Light/LightSorting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSorting : MonoBehaviour
+{
+    private PlayerControl player;
+    
+    void Start()
+    {
+        player = FindObjectOfType<PlayerControl>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerControl>() != null)
+            collision.GetComponent<SpriteRenderer>().sortingLayerID = 2;
+    }
+}
