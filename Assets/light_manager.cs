b67309d436345de77a9d8d1e2d9db0be44144344
WenxuanLiao <wl1884@nyu.edu>
2022-10-30 02:18:54 -0400

    light_time_system

---
+++ Assets/light_manager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class light_manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool once = true;
+    public Light2D light;
+    int interpolationFramesCount = 200; // Number of frames to completely interpolate between the 2 positions
+    int elapsedFrames = 0;
+  
+    void Start()
+    {  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time_text.time_2.Minute >= 22)
+        {
+            float interpolationRatio = (float)elapsedFrames / interpolationFramesCount;
+            elapsedFrames = (elapsedFrames + 1) % (interpolationFramesCount + 1);  // reset elapsedFrames to zero after it reached (interpolationFramesCount + 1)
+            light.intensity = 0.2f;
+
+           once = false;
+
+        }
+    }
+}
