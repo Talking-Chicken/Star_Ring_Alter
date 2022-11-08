03c162ebd619f127abea6448caf3d21f6c2b2aa7
WenxuanLiao <wl1884@nyu.edu>
2022-11-08 01:53:08 -0500

    Nav mesh update

--- Assets/AI_movement_control.cs
+++ Assets/AI_movement_control.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AI_movement_control : MonoBehaviour
 {
@@ -10,19 +11,23 @@ public class AI_movement_control : MonoBehaviour
     Rigidbody2D rbody;
     public Vector2 dir { get; private set; } //direction that player is facing [cannot be (0,0)]
     public static Vector2 movement = new Vector2(0, 0); //direction that player is going [can be (0,0)]
-
+    NavMeshAgent agent;
     private PlayerControl player; //custom script
     Vector2 currentPos;
    public  Vector2 targetPos;
     Vector2 differencePos;
+    Vector2 last_Pos;
+    Vector2 current_Pos;
 
     private void Awake()
     {
-      
+      agent = GetComponentInChildren<NavMeshAgent>();
         isoRenderer = GetComponentInChildren<Isometric_AI_Render>();
-
+        last_Pos = transform.position;
         player = GetComponent<PlayerControl>();
         rbody = GetComponent<Rigidbody2D>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
 
@@ -30,19 +35,22 @@ public class AI_movement_control : MonoBehaviour
     void FixedUpdate()
     {
         setDir();
-        currentPos = rbody.position;
+        last_Pos = currentPos;
+        currentPos = transform.position;
         
      
           //  Vector2 movement = inputVector * movementSpeed;
        //     Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
             isoRenderer.SetDirection(movement);
-        differencePos = targetPos - currentPos;
-        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime*movementSpeed;
-        rbody.MovePosition(newPos);
+        differencePos = currentPos - last_Pos;
+      //  Vector2 newPos = currentPos + movement * Time.fixedDeltaTime*movementSpeed;
+        agent.SetDestination(targetPos);
+        //rbody.MovePosition(newPos);
 
     }
     void setDir() //give dir a Vector2 value, according to WASD that player is pressing
     {
+        Debug.Log(movement);
         if (differencePos.y > 0)
         {
             if (differencePos.x < 0)
