e447c4eaa759a5935f1454b53bd6dd621c55319d
WenxuanLiao <wl1884@nyu.edu>
2022-11-15 14:04:07 -0500

    backup

---
+++ Assets/shadow_follower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shadow_follower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    SpriteRenderer sprite;
+    SpriteRenderer character_sprite;
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        character_sprite = this.GetComponentInParent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sprite.sortingLayerID = character_sprite.sortingLayerID - 1;
+    }
+}
